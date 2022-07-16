6a1d3fa5534ca44b2e00f05e9cda57b79002d94e
vikash <vikash@logicwind.co>
2022-07-16 20:20:10 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+const { withPlugins } = require("next-compose-plugins")
+const withLess = require("next-with-less")
+
+const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: true,
+  images: {
+    deviceSizes: [320, 500, 680, 1040, 2080, 2048, 3120],
+    domains: ["localhost"],
+  },
 }
+
+module.exports = withPlugins(
+  [
+    withLess({
+      lessLoaderOptions: {},
+    }),
+  ],
+  nextConfig
+)
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import "../styles/main.less"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
